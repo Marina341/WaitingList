997fafccdfb57e4672bac7f0dc56fbd79881bdb7
Divoooo <ivan.strok@hotmail.com>
2017-06-03 17:14:34 +0100

    Domagojev Ajax i moje Ocjene

--- app.js
+++ app.js
@@ -10,9 +10,27 @@ app.set('view engine', 'ejs');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 
 app.post('/',urlencodedParser, function(req, res) {
- var inputi=[req.body.item, req.body.item1, req.body.item2,req.body.adress]
+  console.log(" SERVER POST")
+  //var ocjene=[req.body.ustanovaInp,req.body.zahvatInp,req.body.spol,req.body.age,req.body.rating_input_1,req.body.rating_input_2,req.body.rating_input_3];
+  var inputi=[req.body.item, req.body.item1, req.body.item2];
+  console.log(inputi);
+//  console.log(ocjene);
+
   logic.reloadTable(res,inputi);
+
+  //  logic.rateIt(res,ocjene);
+
+
 });
+app.post('/rate',urlencodedParser, function(req, res) {
+  console.log(" SERVER POST")
+  var ocjene=[req.body.ustanovaInp,req.body.zahvatInp,req.body.rating_input_1,req.body.rating_input_2,req.body.rating_input_3,req.body.spol,req.body.age,];
+console.log(ocjene);
+ logic.rateIt(res,ocjene);
+
+
+});
+
 
 app.get('/', function(req, res) {
   logic.loadtable(res);
@@ -21,7 +39,7 @@ app.get('/', function(req, res) {
 app.get('/', function(req, res) {
   res.render('index');
 }); */
-
+/*
 var data=[];
 app.get('/search',function(req,res){
     var query = 'select mydb.kzn.ime from mydb.kzn where mydb.kzn.ime like "%'+req.query.key+'%"';
@@ -34,7 +52,7 @@ app.get('/search',function(req,res){
           res.end(JSON.stringify(data));
     });
     console.log(data);
-});
+});*/
 
 app.listen(8080);
 console.log('Connected at localhost:8080');
